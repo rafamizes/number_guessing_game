12ae6fce01c803f8c62705ba632afe7591b59897
rafael <r.amizes@gmail.com>
2020-09-10 10:16:00 -0300

    fix: off-by-one attempts error

--- src/attempts/attempts.cpp
+++ src/attempts/attempts.cpp
@@ -4,8 +4,8 @@ Attempts::Attempts(VerboseDiff vdiff, int max) : _vdiff{vdiff}, _max{max} {}
 
 bool Attempts::matches() const {
   int attempt = 0;
-  while (attempt <= _max && _vdiff.number() != 0) {
+  while (attempt < _max && _vdiff.number() != 0) {
     ++attempt;
   }
-  return attempt <= _max;
+  return attempt < _max;
 }
